ebf8ca4d09168252bc8083233ff2d3fa55e91fcf
Brett Moore & Bojana Skarich <student@epicodus.com>
2015-04-16 11:06:57 -0700

    Add functionality so that one can add multiple addresses to one contact.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
+    $("#add-address").click(function(event) {
+        $("#new-addresses").append('<div class="new-address">' +
+                                    '<div class="form-group">' +
+                                    '<label for="new-street">Street</label>' +
+                                    '<input type="text" class="form-control new-street">' +
+                                    '</div>' +
+                                    '<div class="form-group">' +
+                                    '<label for="new-city">City</label>' +
+                                    '<input type="text" class="form-control new-city">' +
+                                    '</div>' +
+                                    '<div class="form-group">' +
+                                    '<label for="new-state">State</label>' +
+                                    '<input type="text" class="form-control new-state">' +
+                                    '</div>' +
+                                    '</div>');
+    });
+
     $("form#new-contact").submit(function(event) {
         event.preventDefault();
+
 
 
         var inputtedFirstName = $("input#new-first-name").val();
@@ -18,45 +36,24 @@ $(document).ready(function() {
 
         $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
 
-        $("input#new-first-name").val("");
-        $("input#new-last-name").val("");
-        $("input.new-street").val("");
-        $("input.new-city").val("");
-        $("input.new-state").val("");
-
 
         $(".contact").last().click(function() {
-
-          $("#show-contact").show();
+            $("#show-contact").show();
           $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
           $(".first-name").text(newContact.firstName);
           $(".last-name").text(newContact.lastName);
+
           $("ul#addresses").text("");
           newContact.addresses.forEach(function(address) {
              $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + "</li>");
-             });
+        });
       });
 
-
-
-          $("#add-address").click(function() {
-              $("#new-addresses").append('<div class="new-address">' +
-                                          '<div class="form-group">' +
-                                          '<label for="new-street">Street</label>' +
-                                          '<input type="text" class="form-control new-street">' +
-                                          '</div>' +
-                                          '<div class="form-group">' +
-                                          '<label for="new-city">City</label>' +
-                                          '<input type="text" class="form-control new-city">' +
-                                          '</div>' +
-                                          '<div class="form-group">' +
-                                          '<label for="new-state">State</label>' +
-                                          '<input type="text" class="form-control new-state">' +
-                                          '</div>' +
-                                          '</div>');
-          });
-
-
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+      $("input.new-street").val("");
+      $("input.new-city").val("");
+      $("input.new-state").val("");
 
     });
 });
